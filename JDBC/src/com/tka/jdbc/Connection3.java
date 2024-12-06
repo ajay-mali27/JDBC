e897d4b422a6b21a0a7b317791f4d58b6ce8b09f
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-06 09:49:01 +0530

    Temp

--- JDBC/src/com/tka/jdbc/Connection3.java
+++ JDBC/src/com/tka/jdbc/Connection3.java
@@ -4,19 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class P2_Connection {
-//Same code without Try and Catch
+public class Connection3 {
 
 	public static void main(String[] args) throws SQLException {
 
-//	Load the Driver
-		String url = "jdbc:mysql://localhost:3306/student";
-	
+		String url = "jdbc:mysql://localhost:3306/mca";
 		String username = "root";
-		String password = "striver";
+		String password = "Striver";
 
 		Connection con = DriverManager.getConnection(url, username, password);
 		System.out.println("Connection Succesful");
-
 	}
 }
