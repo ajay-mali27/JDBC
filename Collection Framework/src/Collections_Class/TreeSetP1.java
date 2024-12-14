01b43dfecf0fa49b2ca04ca0db53095f39f7ffcf
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-14 16:31:23 +0530

    TreeSet Eg

---
+++ Collection Framework/src/Collections_Class/TreeSetP1.java
@@ -0,0 +1,17 @@
+package Collections_Class;
+
+import java.util.TreeSet;
+
+public class TreeSetP1 {
+	public static void main(String[] args) {
+
+		TreeSet<String> t1 = new TreeSet<>();
+		t1.add("Ajay");
+		t1.add("Prerana");
+		t1.add("Pallavi");
+		t1.add("Sakshi");
+		t1.add("Xavior");
+		
+		System.out.println(t1);
+	}
+}
