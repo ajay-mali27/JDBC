5727a9218a0841b0826a08940487a3c6588bbfce
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-14 16:34:06 +0530

    LinkedHashSet Program

---
+++ Collection Framework/src/Collections_Class/LinkedHashSetP1.java
@@ -0,0 +1,15 @@
+package Collections_Class;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetP1 {
+	public static void main(String[] args) {
+
+		LinkedHashSet<Integer> LK = new LinkedHashSet<>();
+		LK.add(78);
+		LK.add(45);
+		LK.add(21);
+		LK.add(7);
+		System.out.println(LK);
+	}
+}
